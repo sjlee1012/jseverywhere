8d495e8a3fe1e1fcac2766fe4813a6458f3e666d
sjlee1012 <sjlee1012@gmail.com>
2022-11-11 19:08:57 +0900

    first

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = process.env.PORT||4000;
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!'
+    }
+}
+
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.start().then(() => {
+    server.applyMiddleware({ app , path: '/api'});
+    app.listen({ port }, () => {
+        console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+    });
+});
+
+
+// server.applyMiddleware({ app, path: '/api' });
+// app.listen({ port }, () => {
+//     console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
+// });
+
+
